29b751c431004dbba10301146e536e927cdb07b1
HatBreakingDad <zjwicksy@gmail.com>
2020-05-03 20:30:39 +1000

    Auto Bug-Fix

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var steamcodeslocal = process.env.steamcodes;
 
 var steamtest = process.env.steamtest.split(",");
 for (i in steamtest) {
-    steamtest.split("~");
+    steamtest[i].split("~");
 }
 console.log(steamtest);
 
