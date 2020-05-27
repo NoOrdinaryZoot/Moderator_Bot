c2f2e7254c239b2d74114f5c82ba6a34e3380c2e
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-05-27 17:20:27 +1000

    Auto Bug-Fix

--- main.js
+++ main.js
@@ -120,11 +120,6 @@ client.on("message", async message => {
             case 'play':
                 console.log(storage.queue);
                 execute(message, storage.queue);
-                // play(message, storage.queue.get[message.guild.id]);
-                // if (storage.queue.get(message.guild.id) == 0) {
-
-                // }
-                // storage.queue.get(message.guild.id).push(args[0])
                 return;
             case 'queue':
                 message.channel.send(`${storage.queue.get[message.guild.id].join('\n')}`);
@@ -441,14 +436,13 @@ function play(guild, song) {
 //     });
 // }
 
-function addVideo(term, server) {
+function addVideo(term) {
     youTube.search(term, 1, function (error, result) {
         if (error) {
             console.log(error);
         } else {
             console.log(result.items[0].id.videoId);
             if (result.items[0].id.videoId) {
-                process.env.queues
                 messageTerm.channel.send(`https://youtube.com/watch?v=${result.items[0].id.videoId}`);
                 return;
             }
