f5444fd1bfb75ee8cc6546e60ae488034c3298fe
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-03 08:08:18 +1000

    Auto Bug-Fix

--- commands/mode.js
+++ commands/mode.js
@@ -5,6 +5,7 @@ module.exports = {
     execute(message, args) {
         const config = require('./config.json');
         console.log(args[0].toLowerCase());
+        return;
         console.log(config.modeIndex.indexOf(args[0].toLowerCase()) != -1);
         console.log(config.modeIndex);
         message.channel.send(config.modeIndex.join(' '));
