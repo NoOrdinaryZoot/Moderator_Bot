003243c40e5c1924a2499c296f4ac5d60bba61cd
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-05 19:09:49 +1000

    Auto Bug-Fix

---
+++ commands/blacklist.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'blacklist',
+    description: 'Prints out blacklisted words \nSyntax is ```$blacklist```',
+    mode: 'moderate',
+    execute(message, args) {
+        const storage = require('./storage.json');
+
+        const serverFilter = storage.filters.get(message.guild.id);
+
+        if (!serverFilter) {
+            storage.filters.set(message.guild.id, []);
+            return message.channel.send('There is no blacklist present in this server!')
+        }
+
+        try {
+            return message.channel.send(`Blacklisted words include ${serverFilter.join(', ')}`)
+        } catch {
+            return message.channel.send('Error when sending blacklisted words!')
+        }
+
+        message.channel.send('This is a template command!\nNothing to see here!')
+
+        //Whatever you want down here
+        //message.channel.send is generally a good idea.
+    }
+};
